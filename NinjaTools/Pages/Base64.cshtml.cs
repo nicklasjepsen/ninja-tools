fb6609842f7e19c8c9f45ff8f686980e59db1a37
Nicklas Møller Jepsen <nicklas.m.jepsen@gmail.com>
2022-07-13 11:19:19 +0200

    move shared elements into partial view

--- NinjaTools/Pages/Base64.cshtml.cs
+++ NinjaTools/Pages/Base64.cshtml.cs
@@ -1,13 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NinjaTools.Pages.Shared;
 
 namespace NinjaTools.Pages
 {
     public class Base64Model : PageModel
     {
-        
+        public Base64TextAreaModel InputVm { get; set; }
+        public Base64TextAreaModel EncodedVm { get; set; }
+
+        public Base64Model()
+        {
+            InputVm = new Base64TextAreaModel
+            {
+                Name = "input"
+            };
+            EncodedVm = new Base64TextAreaModel
+            {
+                Name = "encoded"
+            };
+        }
+
         public void OnGet()
         {
-            // Method needed for Razor pages
+            
         }
+        public IActionResult OnGetPartial() =>
+            Partial("_Base64TextArea");
+       
     }
 }
---
+++ NinjaTools/Pages/Shared/_Base64TextArea.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NinjaTools.Pages.Shared
+{
+    public class Base64TextAreaModel : PageModel
+    {
+        public string Name { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}
