35d8823d1366ba291851fc18a42b8e45b1fd1f88
Nicklas Møller Jepsen <nicklas.m.jepsen@gmail.com>
2022-07-13 07:08:28 +0200

    perform encoding/decoding client side

--- NinjaTools/Pages/Base64.cshtml.cs
+++ NinjaTools/Pages/Base64.cshtml.cs
@@ -1,15 +1,9 @@
-using System.Text;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace NinjaTools.Pages
 {
     public class Base64Model : PageModel
     {
-        public string Encoded { get; set; }
-        private string input;
-
-        public string Input { get; set; }
         
         public void OnGet()
         {
